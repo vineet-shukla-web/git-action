1aeb959b42464f3c913f41a7ba17c4ef66bf5bcd
Vineet Kumar Shukla <vineet.10729184@ltimindtree.com>
2025-09-28 18:10:40 +0530

    test

--- app.test.js
+++ app.test.js
@@ -4,7 +4,7 @@ const app = require('./app');
 describe('GET /', () => {
   it('should return Hello, World!', async () => {
     const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(201);
     expect(res.body).toEqual({ message: 'Hello, World!' });
   });
 });
